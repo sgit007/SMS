0766764fbb5e5043b6ac7ba61ed9b98b2413b895
Jiajian <jiajian@jiajiandemacbook-air.local>
2019-08-10 00:04:25 +1000

    Initial Commit

---
+++ SMS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SMS
+//
+//  Created by Jiajian on 2019/8/10.
+//  Copyright © 2019年 Codenerds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
